e39dab74db68beedb16290656fa148a0a6b323b2
Robson <robsonshockwavegithub@gmail.com>
2021-09-01 14:05:11 -0300

    adicionando userContext

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
 import { Home } from './Components/Home';
 import { Login } from './Components/Login/Login';
+import { UserStorage } from './userContext';
 
 function App() {
   return (
     /* <div><Api /></div> */
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes >
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+      <UserStorage >
+          <Header />
+          <Routes >
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
