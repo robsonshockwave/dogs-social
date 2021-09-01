4d7d5a85d0c1ca9387e3e7774835084acc821af0
Robson <robsonshockwavegithub@gmail.com>
2021-09-01 13:11:07 -0300

    hook useForm e validação

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-export const Input = ({label, type, name}) => {
+export const Input = ({label, type, name, value, onChange, error, onBlur}) => {
     return (
         <div className={styles.wrapper}>
             <label htmlFor={name} className={styles.label}>{label}</label>
-            <input className={styles.input} id={name} name={name} type={type}/>
-            <p className={styles.error}>Error</p>
+            <input 
+                className={styles.input} 
+                id={name} 
+                name={name} 
+                type={type} 
+                onChange={onChange} 
+                value={value} 
+                onBlur={onBlur}
+            />
+            {error && <p className={styles.error}>{error}</p>}
         </div>
     ) 
 }
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,35 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Input } from '../Forms/Input';
 import { Button } from '../Forms/Button';
+import { UseForm } from '../../Hooks/UseForm';
 
 export const LoginForm = () => {
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    const username = UseForm();
+    const password = UseForm();
 
     function handleSubmit(event) {
         event.preventDefault();
-        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        })
-        .then((response) => {
-            console.log(response);
-            return response.json();
-        })
-        .then((json) => {
-            console.log(json);
-        });
+
+        if(username.validate() && password.validate()) {
+            fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(),
+            })
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((json) => {
+                console.log(json);
+            });
+        }
     }
 
     return (
         <section>
         <h1>Login</h1>
         <form action="" onSubmit={handleSubmit}>
-            <Input label="Usuário" type="text" name="username"/>
-            <Input label="Senha" type="password" name="password"/>
+            <Input label="Usuário" type="text" name="username" {...username}/>
+            <Input label="Senha" type="password" name="password" {...password}/>
             <Button>Entrar</Button>
         </form>
         <Link to="/login/criar">Cadastro</Link>
