0d712ad417ea039a08a46ebd2fe88766d30b1028
Robson <robsonshockwavegithub@gmail.com>
2021-09-03 13:42:19 -0300

    componente ProtectedRoute (importante) e User

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
+import { ProtectedRoute } from './Components/Helper/ProtectedRoute';
 import { Home } from './Components/Home';
 import { Login } from './Components/Login/Login';
+import { User } from './Components/User/User';
 import { UserStorage } from './userContext';
 
 function App() {
@@ -17,7 +19,8 @@ function App() {
           <Header />
           <Routes >
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />}/>
           </Routes>
           <Footer />
         </UserStorage>
