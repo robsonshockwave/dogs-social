4b91dcff1d55ea7e5849a8bd2ffa4985262fd17f
Robson <robsonshockwavegithub@gmail.com>
2021-09-03 17:24:08 -0300

    estruturacao UserPhotoPost e Api/estilizacao UserPhotoPost

--- src/Hooks/UseForm.js
+++ src/Hooks/UseForm.js
@@ -7,9 +7,12 @@ const types = {
     },
     password: {
         regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
-        message:
-        'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+        message: 'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
     },
+    number: {
+        regex: /^\d+$/,
+        message: 'Utilize números apenas',
+    }
 }
 
 export const UseForm = (type) => {
