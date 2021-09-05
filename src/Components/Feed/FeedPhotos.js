522970b5d9e0f257b3c85acc840a8d4c91d79cb7
Robson <robsonshockwavegithub@gmail.com>
2021-09-04 23:07:57 -0300

    componente Photo, PhotoContent, PhotoComments/estruturacao Photo, PhotoContent/ OBS: CRIADO COMPONENT Loading no commit anterior

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import {Error} from '../Helper/Error'
 import Loading from '../Helper/Loading'
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
 
     const {data, loading, error, request} = useFetch();
 
@@ -24,7 +24,9 @@ const FeedPhotos = () => {
     if(data) { 
         return (
             <ul className={`${styles.feed} animeLeft`}>
-                {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo} />)}
+                {data.map((photo) => (
+                    <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto}/>
+                ))}
             </ul>
             )
     } else {
