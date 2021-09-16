3db8395e4e6c5c9db93fd2c27a3c44a30105fcd1
Robson Arruda <robsonshockwavegithub@gmail.com>
2021-09-15 23:31:30 -0300

    Estruturação LoginPasswordLost

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { PASSWORD_LOST } from '../../Api'
+import { useFetch } from '../../Hooks/useFetch'
+import { UseForm } from '../../Hooks/UseForm'
+import { Button } from '../Forms/Button'
+import { Input } from '../Forms/Input'
+import { Error } from '../Helper/Error'
 
 export const LoginPasswordLost = () => {
+    const login = UseForm();
+    const {data, loading, error, request} = useFetch();
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        if(login.validate()) {
+            const { url, options } = PASSWORD_LOST({login: login.value, url: window.location.href.replace('perdeu', 'resetar')});
+            const { json } = await request(url, options);
+        }
+    }
+
     return (
-        <div>
-            LoginPasswordLost
-        </div>
+        <section>
+            <h1 className="title">Perdeu a senha?</h1>
+            {data ? (
+                <p style={{color: '#4c1'}}>{data}</p>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <Input label="Email / Usuário" type="text" name="login" {...login} />
+                    {loading ? <Button disabled>Enviando...</Button> : <Button>Enviar e-mail</Button>}
+                </form>
+            )}
+
+            <Error error={error} />
+        </section>
     )
 }
