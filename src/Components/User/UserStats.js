306ae91c152d3d216d5db943e85a58da02784a4c
Robson Arruda <robsonshockwavegithub@gmail.com>
2021-09-16 21:13:08 -0300

    Componente userStatsGraph/Estruturação UserStats, UserStatsGraphs

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import Head from '../Helper/Head'
+import { useFetch } from '../../Hooks/useFetch'
+import { GET_STATS } from '../../Api'
+import Loading from '../Helper/Loading'
+import { Error } from '../Helper/Error'
+import UserStatsGraphs from './UserStatsGraphs'
 
 const UserStats = () => {
-    return (
-        <div>
-            <Head title="Estatísticas" />
-            estatisticas
-        </div>
-    )
+    const {data, error, loading, request} = useFetch();
+
+    React.useEffect(() => {
+        async function getData() {
+            const { url, options } = GET_STATS();
+            await request(url, options);
+        }
+        getData();
+    }, [request]);
+
+    if(loading) return <Loading />
+    if(error) return <Error error={error} />
+    if(data) {
+        return (
+            <div>
+                <Head title="Estatísticas" />
+                <UserStatsGraphs data={data} />
+            </div>
+        )
+    } else {
+        return null
+    }
 }
 
 export default UserStats
